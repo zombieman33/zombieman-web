7e15b84cef463e222f9bd0f750928dcd412d2690
zombieman <113766985+zombieman33@users.noreply.github.com>
2023-06-06 01:34:06 +0200

    Add files via upload

--- scripts/website.js
+++ scripts/website.js
@@ -1,14 +1,35 @@
-document.querySelector('.calculator-button')
-    .addEventListener('click', () => {
-        window.open('calculator.html');
-    });
+var dropdownContent = document.getElementById('dropdownContent');
+var searchInput = document.getElementById('searchInput');
+var searchEngineBtn = document.getElementById('searchEngineBtn');
 
-document.querySelector('.rock-paper-scissors-button')
-    .addEventListener('click', () => {
-        window.open('rock-paper-scissors.html');
-    });
+function toggleDropdown() {
+  dropdownContent.classList.toggle('show');
+}
 
-document.querySelector('.todo-list-button')
-    .addEventListener('click', () => {
-        window.open('todo-list.html');
-    });
+function setSearchEngine(searchUrl, searchEngine) {
+  searchInput.dataset.searchUrl = searchUrl;
+  searchEngineBtn.textContent = searchEngine;
+  toggleDropdown();
+}
+
+searchInput.addEventListener('keyup', function(event) {
+  if (event.key === 'Enter') {
+    search();
+  }
+});
+
+function search() {
+  var query = searchInput.value.trim();
+  var searchUrl = searchInput.dataset.searchUrl || 'https://www.google.com/search?q=';
+  if (query !== '') {
+    var domainRegex = /^(https?:\/\/)?([a-zA-Z0-9.-]+)(\.[a-zA-Z]{2,})(\/.*)?$/;
+    var match = query.match(domainRegex);
+    if (match) {
+      var url = (match[1] ? '' : 'https://') + match[2] + match[3] + (match[4] || '');
+      window.open(url, '_blank');
+    } else {
+      searchUrl += encodeURIComponent(query);
+      window.open(searchUrl, '_blank');
+    }
+  }
+}
